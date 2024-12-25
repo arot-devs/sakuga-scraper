49a3fd8ca03f15d184e8b900623f8cc90fd8b339
yada <trojblue@gmail.com>
2024-12-25 09:05:06 +0000

    feat: adding working video tagging code

---
+++ run_scraper.py
@@ -0,0 +1,14 @@
+"""
+Setup:
+- clone repo
+- poetry install
+- poetry shell
+- python run_scraper.py
+
+"""
+
+from sakuga_scraper import SakugaScraper
+
+scraper = SakugaScraper(root_dir='./data/')
+post_ids = [i for i in range(10000, 100000)]
+scraper.scrape_posts(post_ids)
